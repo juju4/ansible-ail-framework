a4ed10a6efcc1874dc721fc158290d9f111bcc7d
juju4 <juju4@users.noreply.github.com>
2017-02-04 10:54:54 -0500

    use reviewed redis role without service activation as ail use multiple config and supervisor

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,7 +13,8 @@ fi
 [ ! -d $rolesdir/juju4.tlsh ] && git clone https://github.com/juju4/ansible-tlsh $rolesdir/juju4.tlsh
 [ ! -d $rolesdir/juju4.redis-leveldb ] && git clone https://github.com/juju4/ansible-redis-leveldb $rolesdir/juju4.redis-leveldb
 [ ! -d $rolesdir/juju4.pystemon ] && git clone https://github.com/juju4/ansible-pystemon $rolesdir/juju4.pystemon
-[ ! -d $rolesdir/geerlingguy.redis ] && git clone https://github.com/geerlingguy/ansible-role-redis.git $rolesdir/geerlingguy.redis
+#[ ! -d $rolesdir/geerlingguy.redis ] && git clone https://github.com/geerlingguy/ansible-role-redis.git $rolesdir/geerlingguy.redis
+[ ! -d $rolesdir/geerlingguy.redis ] && git clone https://github.com/juju4/ansible-role-redis.git $rolesdir/geerlingguy.redis
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.ail-framework ] && ln -s ansible-ail-framework $rolesdir/juju4.ail-framework
 [ ! -e $rolesdir/juju4.ail-framework ] && cp -R $rolesdir/ansible-ail-framework $rolesdir/juju4.ail-framework
