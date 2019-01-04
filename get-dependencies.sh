9929371b0f8d6ada25b4394db8dceeaa849c5db4
Your Name <you@example.com>
2019-01-04 09:12:35 +0000

    galaxy lint

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,7 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 [ ! -d $rolesdir/juju4.faup ] && git clone https://github.com/juju4/ansible-faup $rolesdir/juju4.faup
 [ ! -d $rolesdir/juju4.tlsh ] && git clone https://github.com/juju4/ansible-tlsh $rolesdir/juju4.tlsh
 [ ! -d $rolesdir/juju4.redis-leveldb ] && git clone https://github.com/juju4/ansible-redis-leveldb $rolesdir/juju4.redis-leveldb
@@ -17,9 +17,8 @@ fi
 #[ ! -d $rolesdir/geerlingguy.redis ] && git clone https://github.com/geerlingguy/ansible-role-redis.git $rolesdir/geerlingguy.redis
 [ ! -d $rolesdir/geerlingguy.redis ] && git clone https://github.com/juju4/ansible-role-redis.git $rolesdir/geerlingguy.redis
 ## galaxy naming: kitchen fails to transfer symlink folder
-#[ ! -e $rolesdir/juju4.ail-framework ] && ln -s ansible-ail-framework $rolesdir/juju4.ail-framework
-[ ! -e $rolesdir/juju4.ail-framework ] && cp -R $rolesdir/ansible-ail-framework $rolesdir/juju4.ail-framework
+#[ ! -e $rolesdir/juju4.ail_framework ] && ln -s ansible-ail-framework $rolesdir/juju4.ail_framework
+[ ! -e $rolesdir/juju4.ail_framework ] && cp -R $rolesdir/ansible-ail-framework $rolesdir/juju4.ail_framework
 
 ## don't stop build on this script return code
 true
-
