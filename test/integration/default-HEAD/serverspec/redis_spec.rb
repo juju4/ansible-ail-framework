6fc83d9c908153238fe0fa62cf7f1ad4353ab460
juju4 <juju4@users.noreply.github.com>
2019-12-07 20:20:45 -0500

    test/default*: check ardb/redis6382 log file

--- test/integration/default-HEAD/serverspec/redis_spec.rb
+++ test/integration/default-HEAD/serverspec/redis_spec.rb
@@ -7,3 +7,8 @@ describe process("redis-server") do
   it { should be_running }
 end
 
+describe file('/var/log/supervisor/ail-redis6382.log') do
+  its(:content) { should match /RocksDB version:/ }
+  its(:content) { should match /Creating manifest 1/ }
+  its(:content) { should_not match /ERROR/ }
+end
